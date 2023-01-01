b61928f3f8f48d780c7b10807cccd08a480e2bc1
Master Piyaboot Prasertsuwan <71850259+ballbeamboy2@users.noreply.github.com>
2023-01-02 00:15:24 +0100

    Add files via upload

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿//interpol use to concentrate strings without using + 
+
+    int a = 5;
+    int b = 3;
+
+string C = "ad";
+string D = "da";
+   string message = $"{a} plus {b} is equal to {a + b}";
+    Console.WriteLine($"WDWDWDWD");
+Console.WriteLine(message);
