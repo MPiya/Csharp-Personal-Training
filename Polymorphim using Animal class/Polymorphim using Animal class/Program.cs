5000fb67001917bf4a435ea77fc922e91d4ba701
Master Piyaboot Prasertsuwan <71850259+ballbeamboy2@users.noreply.github.com>
2023-01-04 01:00:47 +0100

    r
    heh

---
+++ Polymorphim using Animal class/Polymorphim using Animal class/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Animal
+{
+    public virtual void MakeNoise()
+    {
+        Console.WriteLine("Some generic animal noise");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeNoise()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeNoise()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        animal.MakeNoise(); // Output: "Some generic animal noise"
+
+        Animal dog = new Dog();
+        dog.MakeNoise(); // Output: "Woof!"
+
+        Animal cat = new Cat();
+        cat.MakeNoise(); // Output: "Meow!"
+    }
+}
